69de0dc432e1f2a62c1175b360c45404016af1eb
Cadu Silva <contato@cadusilva.com>
2024-01-15 02:25:00 -0300

    Pequeno ajuste estético

--- update.py
+++ update.py
@@ -16,7 +16,7 @@ def now_listening_str():
     if not now_playing: return None
     track_name = now_playing.get_title()
     artist_name = now_playing.get_artist().get_name()
-    return track_name + ' - ' + artist_name
+    return track_name + ' • ' + artist_name
 
 def update_fields():
     mastodon = Mastodon(access_token=MAST_ACC_TOKEN, api_base_url=INSTANCE_URL)
@@ -24,12 +24,12 @@ def update_fields():
     has_now_listening_field = False
     for row in profile_fields:
         del row['verified_at']
-        if row['name'] == 'Ouvindo agora 🔊':
+        if row['name'] == 'Ouvindo agora':
             has_now_listening_field = True
             row['value'] = now_listening_str()
 
     if not has_now_listening_field:
-        profile_fields.append({ 'name': 'Ouvindo agora 🔊', 'value': now_listening_str()})
+        profile_fields.append({ 'name': 'Ouvindo agora', 'value': now_listening_str()})
     new_profile_fields = [row for row in profile_fields if row['value'] is not None]
     new_profile_tuples = [tuple(row.values()) for row in new_profile_fields]
     result = mastodon.account_update_credentials(fields = new_profile_tuples)
